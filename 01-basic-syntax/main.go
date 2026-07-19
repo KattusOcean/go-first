3d9459e90f31f8285f2b1299a2ac71a3c065e6ef
Kattus <alexvalverdetorres2006@gmail.com>
2026-07-19 13:01:01 +0200

    hello world

---
+++ 01-basic-syntax/main.go
@@ -0,0 +1,10 @@
+// A package must always be declared
+package main
+
+// Import statement of a format package, necessary
+// for IO operations like scanf, printf, Println
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
